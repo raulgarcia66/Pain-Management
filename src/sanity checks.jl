ca03fa855c92e0eeb533f2c32cc095d61cad86b4
Raul Garcia <rauljgarcia66@gmail.com>
2023-06-23 19:30:34 -0500

    Monotoneness of state componets

--- src/sanity checks.jl
+++ src/sanity checks.jl
@@ -27,7 +27,6 @@ policy_matrix = hcat(map(t -> actions[π[t]], 1:T)...)
 ##################################################################################
 ##################################################################################
 # Monotoneness of rewards over time
-T = 6
 time_states = [Int[] for _ in eachindex(states), _ in eachindex(actions)]
 
 for i in eachindex(states), a in action_sets[i]
@@ -56,13 +55,12 @@ for i in eachindex(states), a in action_sets[i]
 end
 
 collect(eachrow(time_states))
-
+time_states[2,1]
 # Summary: Def not monotone. Oscillates.
 
 ##################################################################################
 ##################################################################################
 # Monotoneness of value function
-T = 6
 time_states = [Int[] for _ in eachindex(states)]
 
 for i in eachindex(states)
@@ -96,5 +94,137 @@ collect(eachrow(time_states))
 
 ##################################################################################
 ##################################################################################
-# Vary one state component, fix others. Check for one period at a time
+# Vary one state component, fix others. Check value function for one period at a time
 
+states = ["[0,P,MM]", "[0,A,MM]", "[0,G,MM]", "[0,P,MS]", "[0,A,MS]", "[0,G,MS]",
+    "[1,P,MM]", "[1,A,MM]", "[1,G,MM]", "[1,P,MS]", "[1,A,MS]", "[1,G,MS]",
+    "[2,P,MM]", "[2,A,MM]", "[2,G,MM]", "[2,P,MS]", "[2,A,MS]", "[2,G,MS]"]
+num_states = length(states)
+
+actions = ["+2", "+1", "0" , "-1", "-2"]
+num_actions = length(actions)
+
+action_sets = [(1,2,3), (1,2,3), (1,2,3), (1,2,3), (1,2,3), (1,2,3),
+              (2,3,4), (2,3,4), (2,3,4), (2,3,4), (2,3,4), (2,3,4),
+              (3,4,5), (3,4,5), (3,4,5), (3,4,5), (3,4,5), (3,5)]  # state 18 modified
+
+#### Vary first component
+linear_pain_med_vec = [[true for _ = 1:6] for _ = 1:T] # 6 states to check
+initial_trend_pain_med_vec = [["" for _ = 1:6] for _ = 1:T]
+for t = 1:T
+    state_iter = collect(1:6)
+    for s = eachindex(state_iter)
+
+        set = [state_iter[s], state_iter[s]+6, state_iter[s]+12]
+        if u[t][set[1]] > u[t][set[2]]
+            # initial_trend = "decreasing"
+            initial_trend_pain_med_vec[t][s] = "decreasing"
+            if u[t][set[2]] >= u[t][set[3]]
+                # linear_pain_med = true
+                linear_pain_med_vec[t][s] = true
+            else
+                # linear_pain_med = false
+                linear_pain_med_vec[t][s] = false
+            end
+        elseif u[t][set[1]] < u[t][set[2]]
+            # initial_trend = "increasing"
+            initial_trend_pain_med_vec[t][s] = "increasing"
+            if u[t][set[2]] <= u[t][set[3]]
+                # linear_pain_med = true
+                linear_pain_med_vec[t][s] = true
+            else
+                # linear_pain_med = false
+                linear_pain_med_vec[t][s] = true
+            end
+        end
+
+    end
+end
+
+for time = 1:T, state_num = 1:6
+    println("\n(time, state_num) = ($time, $state_num)")
+    linear_pain_med_vec[time][state_num] ? println("Monotone") : println("Nonmonotone")
+    initial_trend_pain_med_vec[time][state_num] == "increasing" ? println("Increasing initially") : println("Increasing initially")
+end
+
+#### Vary second component
+linear_func_vec = [[true for _ = 1:6] for _ = 1:T] # 6 states to check
+initial_trend_func_vec = [["" for _ = 1:6] for _ = 1:T]
+for t = 1:T
+    state_iter = [collect(1:3); collect(7:9); collect(13:15)]
+    state_iter = collect(1:3:18)
+    for s = eachindex(state_iter)
+
+        set = [state_iter[s], state_iter[1]+1, state_iter[s]+2]
+        if u[t][set[1]] > u[t][set[2]]
+            # initial_trend = "decreasing"
+            initial_trend_func_vec[t][s] = "decreasing"
+            if u[t][set[2]] >= u[t][set[3]]
+                # linear_pain_med = true
+                linear_func_vec[t][s] = true
+            else
+                # linear_pain_med = false
+                linear_func_vec[t][s] = false
+            end
+        elseif u[t][set[1]] < u[t][set[2]]
+            # initial_trend = "increasing"
+            initial_trend_func_vec[t][s] = "increasing"
+            if u[t][set[2]] <= u[t][set[3]]
+                # linear_pain_med = true
+                linear_func_vec[t][s] = true
+            else
+                # linear_pain_med = false
+                linear_func_vec[t][s] = true
+            end
+        end
+
+    end
+end
+
+for time = 1:T, state_num = 1:6
+    println("\n(time, state_num) = ($time, $state_num)")
+    linear_func_vec[time][state_num] ? println("Monotone") : println("Nonmonotone")
+    initial_trend_func_vec[time][state_num] == "increasing" ? println("Increasing initially") : println("Increasing initially")
+end
+
+#### Vary third component
+linear_pain_level_vec = [[true for _ = 1:9] for _ = 1:T] # 9 states to check
+initial_trend_pain_level_vec = [["" for _ = 1:9] for _ = 1:T]
+for t = 1:T
+    state_iter = [collect(1:3); collect(7:9); collect(13:15)]
+    for s = eachindex(state_iter)
+
+        set = [state_iter[s], state_iter[1]+3]
+        if u[t][set[1]] > u[t][set[2]]
+            # initial_trend = "decreasing"
+            initial_trend_pain_level_vec[t][s] = "decreasing"
+            # if u[t][set[2]] >= u[t][set[3]]
+            #     # linear_pain_med = true
+            #     linear_pain_level_vec[t][s] = true
+            # else
+            #     # linear_pain_med = false
+            #     linear_pain_level_vec[t][s] = false
+            # end
+        elseif u[t][set[1]] < u[t][set[2]]
+            # initial_trend = "increasing"
+            initial_trend_pain_level_vec[t][s] = "increasing"
+            # if u[t][set[2]] <= u[t][set[3]]
+            #     # linear_pain_med = true
+            #     linear_pain_level_vec[t][s] = true
+            # else
+            #     # linear_pain_med = false
+            #     linear_pain_level_vec[t][s] = true
+            # end
+        end
+
+    end
+end
+
+for time = 1:T, state_num = 1:9
+    println("\n(time, state_num) = ($time, $state_num)")
+#     linear_pain_level_vec[time][state_num] ? println("Monotone") : println("Nonmonotone")
+    initial_trend_pain_level_vec[time][state_num] == "increasing" ? println("Increasing") : println("Decreasing")
+end
+
+
+# TODO: Do the state component as above over time as well
